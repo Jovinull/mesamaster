77242f3cb3d85a92a57df63f717fbe7ec1eb9b7b
jovinull <felipejovinogamerplay@gmail.com>
2026-04-02 08:05:16 -0300

    feat: start do projeto

---
+++ src/main/java/com/mesamaster/mesamaster/MesamasterApplication.java
@@ -0,0 +1,13 @@
+package com.mesamaster.mesamaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesamasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesamasterApplication.class, args);
+	}
+
+}
